066a18dd0f36ef6e488608c307db4580ef618c01
sandeep jangra <jangra.1202@gmail.com>
2016-06-05 10:41:09 +0530

    Linked previous and upcoming pages

--- main.js
+++ main.js
@@ -7,12 +7,21 @@ app.use(express.static(__dirname+"/static"));
 app.set('view engine','pug')
 app.set('views',__dirname+'/views')
 
-app.get('/event/:club_name', function (req, res) {
+app.get('/:club_name/home', function (req, res) {
 	var club_name = req.params.club_name
 	//console.log(db[club_name])
 	res.render('event_home',db[club_name])
 })
 
+app.get('/:club_name/event/:query',function (req,res) {
+	var club_name = req.params.club_name;
+	var query = req.params.query;
+	if(query=="previous") 
+		res.render('previousevent')
+	else if(query=="upcoming")
+		res.render('upcomingevent')
+	}
+)
 app.listen(8081, function () {
-  	console.log("Example app listening at http://localhost:8081")
+  	console.log("Server listening at http://localhost:8081")
 	})
