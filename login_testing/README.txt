664b83697b14153482672868b1f0a918ee3c9cdb
sandeep jangra <jangra.1202@gmail.com>
2016-06-05 15:27:20 +0530

    linking of previous event,upcoming event,home page,login page complete
    TODO
    put some nice pics,content
    test and correct the css on all pages
    make it look good

--- login_testing/README.txt
+++
@@ -1,3 +0,0 @@
-A Pen created at CodePen.io. You can find this one at http://codepen.io/gwilakers/pen/xGwLyQ.
-
- This is a basic login form with various animations that happen on certain events. With a little added validation this form could be plugged into any site that needs logins.
--- main.js
+++ main.js
@@ -1,25 +1,47 @@
 var express = require('express');
+var session = require('express-session')
+var bodyParser = require('body-parser');
 var fs = require('fs');
-var db = require('./data.json');
+var club_db = require('./database/club.json');
+var user_db = require('./database/user.json');
 var app = express();
 
+app.use(session({
+	saveUninitialized:false,
+	secret:'mouse on the house',
+	resave:true
+}))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static(__dirname+"/static"));
 app.set('view engine','pug')
 app.set('views',__dirname+'/views')
 
+app.all('/',function(req,res) {
+	switch(req.method){
+	case 'GET':res.render('login');break;
+	case 'POST':if(user_db[req.body.userid].password == req.body.password){
+					req.session.user = {id:req.body.userid}
+					res.redirect("/3ctech/home");
+				}
+				break;
+	}    
+});
+
 app.get('/:club_name/home', function (req, res) {
 	var club_name = req.params.club_name
-	//console.log(db[club_name])
-	res.render('event_home',db[club_name])
+	club_db[club_name].club_name = club_name
+	res.render('event_home',club_db[club_name])
 })
 
 app.get('/:club_name/event/:query',function (req,res) {
+	console.log("user data: ",req.session.user)
 	var club_name = req.params.club_name;
 	var query = req.params.query;
 	if(query=="previous") 
-		res.render('previousevent')
+		res.render('previousevent',{club_name:club_name})
 	else if(query=="upcoming")
-		res.render('upcomingevent')
+		res.render('upcomingevent',{club_name:club_name})
 	}
 )
 app.listen(8081, function () {
